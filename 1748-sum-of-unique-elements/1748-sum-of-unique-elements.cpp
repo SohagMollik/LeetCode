9eaf993e5be4de6fac7ddf50e4e1d39f9ad7711d
Sohag Mollik <sohagmollik871@gmail.com>
2022-07-11 08:46:15 +0600

    Time: 0 ms (100.00%), Space: 8 MB (22.33%) - LeetHub

---
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int sumOfUnique(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();++i){
+            mp[nums[i]]++;
+        }
+        int sum=0;
+        for(auto x: mp){
+            int y=x.second;
+            if(y<=1)sum+=(x.first);
+        }
+        return sum;
+    }
+};
