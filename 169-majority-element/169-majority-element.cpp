e82cb73e7437648cd88c9ad08210e13b3021ab66
Sohag Mollik <sohagmollik871@gmail.com>
2022-07-18 00:05:44 +0600

    Time: 63 ms (5.21%), Space: 19.7 MB (12.88%) - LeetHub

---
+++ 169-majority-element/169-majority-element.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        map<int,int>mp;
+        for(int i=0;i<nums.size();++i){
+            mp[nums[i]]++;
+        }
+        int mx=0,ans;
+        for(auto x: mp){
+            if(x.second >= mx){
+                mx=x.second;
+                ans=x.first;
+            }
+        }return ans;
+    }
+};
