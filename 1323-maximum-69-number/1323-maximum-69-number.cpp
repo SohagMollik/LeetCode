06c1f07246f54b58a65268c8e8ce9d73fc82494b
Sohag Mollik <sohagmollik871@gmail.com>
2022-11-07 11:25:03 +0600

    Time: 0 ms (100.00%), Space: 5.9 MB (82.75%) - LeetHub

---
+++ 1323-maximum-69-number/1323-maximum-69-number.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximum69Number (int num) {
+        string st=to_string(num);
+        for(int i=0;i<st.size();++i){
+            if(st[i]=='6'){
+            st[i]='9';
+            break;
+            }
+            else st[i]=st[i];
+        }
+        int value=stoi(st);
+        return value;
+    }
+};
