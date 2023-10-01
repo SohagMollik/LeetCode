2ca81deb21668eab44105fa80abe71404cd3e6bd
Sohag Mollik <sohagmollik871@gmail.com>
2023-10-01 23:09:25 +0600

    Time: 19 ms (30.66%), Space: 11.8 MB (35.60%) - LeetHub

---
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        
+        string temp="";
+        string ans="";
+        for(int i=0;i<s.length();++i){
+            if(s[i]==' '){
+                reverse(temp.begin(),temp.end());
+                ans+=temp;
+                ans+=' ';
+                temp="";
+            }
+            else{
+                temp+=s[i];
+            }
+        }
+        reverse(temp.begin(),temp.end());
+        ans+=temp;
+       
+        
+        
+        return ans;
+    }
+};
