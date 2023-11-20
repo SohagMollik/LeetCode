a41702cbf43fbae7a086feb7c218bd9bb01a11c8
Sohag Mollik <sohagmollik871@gmail.com>
2023-11-21 02:02:15 +0600

    Time: 203 ms (11.71%), Space: 102.4 MB (91.88%) - LeetHub

--- 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.cpp
@@ -1,62 +1,52 @@
 class Solution {
 public:
-    int garbageCollection(vector<string>& g, vector<int>& t) {
-
-        int p_last=-1;
-        int g_last=-1;
-        int m_last=-1;
- 
-for(int ii=1;ii<t.size();++ii)
-{
-    t[ii]=t[ii-1]+t[ii];
-}
+    int garbageCollection(vector<string>& garbage, vector<int>& travel) {
         
-int p_total=0,g_total=0,m_total=0;
- 
- for(int i=0;i<g.size();++i){
- if(count(g[i].begin(),g[i].end(),'P')>0){
-p_last=i;
-p_total+=count(g[i].begin(),g[i].end(),'P');
- }
-     
-     
-if(count(g[i].begin(),g[i].end(),'G')>0){
-g_last=i;
-g_total+=count(g[i].begin(),g[i].end(),'G');
- }
-     
-     
-if(count(g[i].begin(),g[i].end(),'M')>0){
-m_last=i;
-m_total+=count(g[i].begin(),g[i].end(),'M');
- }
-
-}
-
-int ans=0;
+        for(int ii=1;ii<travel.size();++ii){
+            travel[ii] = travel[ii] + travel[ii-1];
+        }
         
-if(p_last>-1){
-if(p_last>0)ans+=p_total+t[p_last-1];
-    
-else ans+=p_total;
-    
-}
-
-if(g_last>-1){
-if(g_last>0)ans+=g_total+t[g_last-1];
-    
-else ans+=g_total;
-    
-}
+        int metal=0, paper=0, glass=0;
+        int p=-1, m=-1, g=-1;
         
-if(m_last>-1){
-if(m_last>0)ans+=m_total+t[m_last-1];
-    
-else ans+=m_total;
-    
-}
-
-
-return ans;
+        for(int i=0;i<garbage.size();++i){
+            
+            if(count(garbage[i].begin(), garbage[i].end(),'M') > 0){
+                m=i;
+                metal+=count(garbage[i].begin(), garbage[i].end(),'M');
+            }
+            
+            if(count(garbage[i].begin(), garbage[i].end(),'P')>0){
+                p=i;
+                paper+=count(garbage[i].begin(), garbage[i].end(),'P');
+            }
+            
+            if(count(garbage[i].begin(), garbage[i].end(),'G')>0){
+                g=i;
+                glass+=count(garbage[i].begin(), garbage[i].end(),'G');
+            }
+        }
+        
+        int ans=0;
+        
+        if(p>-1){
+            if(p>0)ans+=paper+travel[p-1];
+            
+            else ans+=paper;
+        }
+        
+         if(m>-1){
+            if(m>0)ans+=metal+travel[m-1];
+             
+            else ans+=metal;
+        }
+        
+         if(g>-1){
+            if(g>0)ans+=glass+travel[g-1];
+             
+            else ans+=glass;
+        }
+        
+        return ans;
     }
 };
