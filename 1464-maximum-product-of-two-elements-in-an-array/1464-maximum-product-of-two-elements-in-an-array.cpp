1c3e35977845010599a16f95779776f419961f76
Sohag Mollik <sohagmollik871@gmail.com>
2023-12-12 08:46:48 +0600

    Update 1464-maximum-product-of-two-elements-in-an-array.cpp

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -1,7 +1,23 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-        return (nums[nums.size()-1]-1)*(nums[nums.size()-2]-1);
+        
+       int a=0,b=0,ok;
+        
+        for(int i=0;i<nums.size();++i){
+            if(nums[i]>a){
+                a=nums[i]; 
+                ok=i;
+            }
+        }
+        
+          for(int i=0;i<nums.size();++i){
+            if(nums[i] != a or i != ok){
+                if(b==0)b=nums[i];
+                else if(nums[i]>b)b=nums[i];
+            }
+        }
+        
+        return (a-1)*(b-1);
     }
 };
