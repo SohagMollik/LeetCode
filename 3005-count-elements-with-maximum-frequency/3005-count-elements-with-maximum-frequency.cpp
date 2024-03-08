845549137e68f8728ecf500d6a77de7336ce8323
Sohag Mollik <sohagmollik871@gmail.com>
2024-03-08 09:04:37 +0600

    Time: 0 ms (100.00%), Space: 22 MB (17.21%) - LeetHub

--- 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
+++ 3005-count-elements-with-maximum-frequency/3005-count-elements-with-maximum-frequency.cpp
@@ -3,29 +3,21 @@ public:
     int maxFrequencyElements(vector<int>& nums) {
         
         map<int,int>mp;
-        
-        for(int i=0;i<nums.size();++i){
-            mp[nums[i]]++;
+        for(auto pic: nums){
+            mp[pic]++;
         }
         
-        vector<int>v;
-        for(auto x: mp){
-            int y=x.second;
-            v.push_back(y);
+        int mx=0;
+        for(auto pic: mp){
+            int x=pic.second;
+            mx=max(mx,x);
         }
         
-        sort(v.begin(),v.end(),greater<int>());
         int sum=0;
-        for(int i=1;i<v.size();++i){
-            if(v[i-1]==v[i]){
-                sum+=v[i-1];
-            }
-            else{
-                sum+=v[i-1];
-                break;
-            }
+        for(auto pic: mp){
+            int x=pic.second;
+            if(x==mx)sum+=x;
         }
-        if(v[0]==v[v.size()-1]) {sum+=v[v.size()-1];}
         
         return sum;
     }
