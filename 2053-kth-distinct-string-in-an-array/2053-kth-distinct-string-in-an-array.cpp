a9c53a2d39cfb6122b7b3deb4d3f64a56fbcd78d
Sohag Mollik <sohagmollik871@gmail.com>
2024-08-05 23:07:03 +0600

    Time: 21 ms (30.77%), Space: 18.5 MB (20.16%) - LeetHub

---
+++ 2053-kth-distinct-string-in-an-array/2053-kth-distinct-string-in-an-array.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+        
+        unordered_map<string,int>freq;
+        
+        for(auto str: arr){
+           freq[str]++;
+        }
+        
+        for(auto str: arr){
+            if(freq[str]==1){
+                k--;
+                if(k==0){
+                    return str;
+                }
+            }
+        }
+        
+        return "";
+    }
+};
