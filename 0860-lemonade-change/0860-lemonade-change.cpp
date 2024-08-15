dc05fe2a4002c00bab1fb3ca5ae089b55315507d
Sohag Mollik <sohagmollik871@gmail.com>
2024-08-15 11:46:48 +0600

    Time: 80 ms (22.48%), Space: 86.1 MB (32.71%) - LeetHub

---
+++ 0860-lemonade-change/0860-lemonade-change.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+        int five=0, ten=0, twinty=0;
+        
+        for(int i=0; i<bills.size(); ++i){
+            
+            if(bills[i]==5){
+                five++;
+            }
+            
+            else if(bills[i]==10){
+                ten++;
+                if(five <= 0){
+                    return false;
+                    break;
+                }
+                
+                else{
+                    five--;
+                }
+            }
+            
+            else{  
+                if((five<=0 && ten<=0) or (ten>0 && five<=0) or (five<3 && ten<=0)){
+                   return false;
+                    break;
+                }
+                
+                else if(five>=3 && ten<=0){
+                    five-=3;
+                }
+                
+                else{
+                    five--;
+                    ten--;
+                }
+            }
+
+            
+        }
+        
+         return true;
+      
+    }
+};
