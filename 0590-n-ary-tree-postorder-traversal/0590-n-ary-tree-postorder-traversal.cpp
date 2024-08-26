3fd2285093c40ed698257c373ca2735ec682b839
Sohag Mollik <sohagmollik871@gmail.com>
2024-08-26 09:51:22 +0600

    Time: 18 ms (22.62%), Space: 15.3 MB (49.47%) - LeetHub

---
+++ 0590-n-ary-tree-postorder-traversal/0590-n-ary-tree-postorder-traversal.cpp
@@ -0,0 +1,43 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    
+    void solve(Node* root, vector<int>&v){
+       if(root==NULL){
+           return;
+       }
+        
+        for(auto child: root->children){
+        solve(child,v);
+        }
+        
+        v.push_back(root->val);
+    }
+    
+    vector<int> postorder(Node* root) {
+       vector<int>v;
+       solve(root, v);
+        
+        return v;
+        
+    }
+};
