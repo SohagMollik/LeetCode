ca84369b6f78f4872a2613c9f2a0ad451c8f36cf
Sohag Mollik <sohagmollik871@gmail.com>
2026-01-05 14:32:26 +0600

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
+++ 1975-maximum-matrix-sum/1975-maximum-matrix-sum.cpp
@@ -1,28 +1,23 @@
 class Solution {
 public:
     long long maxMatrixSum(vector<vector<int>>& matrix) {
-        
-        int row = matrix.size();
-        int col = matrix[0].size();
-        
-        long int sum=0;
-        int minValue = INT_MAX, negCount=0;
-        
-        for(int i=0; i<row; ++i){
-            for(int j=0; j<col; ++j){
-                
-                sum+=abs(matrix[i][j]);
-                
+
+        int row=matrix.size();
+        int col=matrix[0].size();
+
+        int sum=0, negCount=0, minValue=INT_MAX;
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]<0)negCount++;
                 minValue = min(minValue, abs(matrix[i][j]));
-                
-                if(matrix[i][j]<0) ++negCount;
-                
+                sum+=abs(matrix[i][j]);
+
             }
         }
+
         
-        
-        if(negCount%2==0) return sum;
-        else return sum-(2*minValue);
-        
+
+        return (negCount%2==0)? sum: sum-(2*minValue);
+
     }
 };
